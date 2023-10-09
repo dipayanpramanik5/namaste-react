acb43ef56e382b68b9265683ed31c19e7c9b1726
drupalwork <dipayan.pramanik2018@gmail.com>
2023-10-09 17:32:54 +0530

    Second episode.

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "heading" }, "Hello world from react");
 console.log(heading);
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement(
-        "div",
-        { id: "child" },
-        [
-            React.createElement("h1", {}, "I am a h1 tag"),
-            React.createElement("p", { className: "ptag" }, "I am a P tag")
-        ]
-    )
-);
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am a h1 tag by Dipayan"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+    ]),
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am a h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+    ]),
+]);
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Namaste React
+
+# parcel
+- Dev build
+- Local Server
+- refresh page without load(HMR- Hot Module Replacement)
+- File watching algorithm (Written in C++)
+- Faster build due to cache
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- consistant hashing
+- Code spliting
+- Differential Bundling - Support older browser
+- Diagonistic
+- Error handling
+- Host app in https
+- Tree Shaking  - Remove unused code
+- Different dev and production build
